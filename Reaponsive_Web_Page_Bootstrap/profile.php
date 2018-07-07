118e67a514d61ba5e6378b1fb2826dbcc684bddc
Nehasoni988 <nehaswrankar988@gmail.com>
2018-07-07 07:37:53 +0530

    Responsive_Web_Page_Bootstrap

---
+++ Reaponsive_Web_Page_Bootstrap/profile.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="profile.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+ 
+<!-- heading-->
+<div class="container-fluid">
+    <div class="jumbotron" style="margin-top: 6px;">
+      <h1>WELCOME&ensp;TO&ensp;<b id="ab">AMITABH &ensp;BACCHAN'S</b>&ensp; OFFICAL &ensp;WEB&ensp; PAGE</h1>
+    </div>
+</div>  
+<!-- heading close-->
+<!--button class -->
+<div class="container-fluid mt-5">
+   <div class="row" id="buttons ">
+       <div class="col-xs-4 col-md-4  col-sm-4 col-lg-4">
+               <button type="button" class="btn btn-danger btn-responsive"><a href="#about"><font color="white">About</font></a></button>
+               <button type="button" class="btn btn-success"><a href="#pc"><font color="white">parents</font></a></button>
+               <button type="button" class="btn btn-warning"><a href="#pc"><font color="white">Childerns</font></a></button>
+               <button type="button" class="btn btn-primary"><a href="#movies"><font color="white">Movies</font></a></button>  
+       </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center mt-0">
+         
+          <span id="red">B</span><span id="blue">i</span><span id="yellow">G</span><span></span><span id="orange">B</span><span id="blue">'s</span>
+          <span id="red">B</span><span id="blue">L</span><span  id="yellow">O</span><span id="orange">g</span>
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id=second>
+                <button type="button" class="btn btn-danger"><a href="#l1"><font color="white">Biogrphy</font></a></button>
+               <button type="button" class="btn btn-info"><a href="#life-story"><font color="white">life-story</font></a></button>
+               <button type="button" class="btn btn-warning"><a href="#songs"><font color="white">Songs</font></a></button>
+                <button type="button" class="btn btn-success"><a href="#awards"><font color="white">Awards</font></a></button>  
+       </div>
+  </div>   
+</div>
+  <!-- button class close-->      
+<div class="container">
+         <div class="media" id="first">
+                <img src="bachhan.jpg" class="rounded img-responsive img-fluid m-3 col-xs-12 col-sm-12 col-md-6 " id="img1">
+             <div class="media-body pl-3 pr-0 " id="about">
+               <b id="pera">A</b>mitabh Bachchan is looking at the camera.
+                Studio publicity photograph of Bachchan in 2009
+                Amitabh Bachchan is an Indian film actor, playback singer, producer and television personality. He made his acting debut in 1969 with Saat Hindustani, and narrated Mrinal Sen's Bhuvan Shome (1969).[1] He later appeared as Dr. Bhaskar Banerjee in Hrishikesh Mukherjee's Anand (1971), for which he won the Filmfare Award for Best Supporting Actor.[1] In 1973, Bachchan played the role of Inspector Vijay Khanna in Prakash Mehra's action film Zanjeer. He has since appeared in several films with the character name "Vijay".[2] During the same year, he appeared in Abhimaan and Namak Haraam. For the latter, he received the Filmfare Award for Best Supporting Actor. Two years later he appeared along with Shashi Kapoor, in Yash Chopra's Deewaar, which earned him the Filmfare Award for Best Actor nomination. He was cited as the "angry young man" for his roles in Deewaar and Zanjeer.[3] Later he starred in Ramesh Sippy's Sholay (1975), which is considered to be one of the greatest Indian films of all time.[4][5] After appearing in the romantic drama Kabhie Kabhie (1976), Bachchan starred in Manmohan Desai's action comedy Amar Akbar Anthony (1977). He won the Filmfare Award for Best Actor for his performance in the latter. He then played dual roles of Don and Vijay in Don (1978).
+                In the 1980s, Bachchan was cast in numerous films, .
+             </div>
+         </div>
+         <!-- extra content1-->
+         <div class="media border mt-4">
+              <div class="media-body pr-5 pt-5 pl-2" id="movies">
+                 <b id='pera'> H</b>e went on to score major hits with movies such as ‘Anand’ (1970) and ‘Namak Haram’ (1973). Amitabh came on to the scene when there was a great deal of disaffection in the country, especially among the educated unemployed. In the subsequent years, he went on to give several major hits including ‘Deewaar’ (1975), in which he played the role of a mafia don, besides ‘Trishul’ (1978), ‘Don’ (1978), ‘Natwarlal’ (1979), ‘Suhaag’ (1979), ‘The Great Gambler’ (1980), ‘Kaalia’ (1981), ‘Silsila’ (1981), ‘Shakti’ (1982).
+              </div>
+                 <img src="ab4.jpg" class="img-responsive img-fluid rounded-circle m-5">
+         </div>
+         <!-- extra content 2-->
+         <div class="media border mt-4">
+                 <img src="ab5.jpg" class="img-responsive img-fluid rounded-circle m-5">
+              <div class="media-body pl-5 pt-4 pr-2" id="awards">
+                 <b id="pera">D</b>eewaar received the Filmfare Best Movie Award of 1975, in addition to six other Filmfare Awards. It was also a "super hit" at the box office, ranking as the fourth highest-grossing Bollywood film of 1975. Indiatimes ranks Deewaar amongst the Top 25 Must See Bollywood Films.
+                 The film had several remakes. It was remade in Telugu as Magaadu (1976), in Tamil as Thee (1981), and in Malayalam as Nathi Muthal Nathi Vare (1983). The Shaw Brothers Studio produced a Hong Kong remake, The Brothers (1979), which in turn inspired John Woo's breakthrough heroic bloodshed film A Better Tomorrow (1986). There was also a Bollywood remake, Aatish: Feel the Fire (1994)
+          
+              </div>
+         </div>
+         <!--extra content 3 -->
+
+          <div class="media border mt-4">
+             
+              <div class="media-body pr-5 pt-5 pl-2" id="life-story">
+                
+                <b id="pera">A</b>lthough Amitabh Bachchan’s unwavering popularity makes it difficult to imagine that he was ever anything but famous, this legendary actor’s success story is all about troughs and crests. Unlike many superstars, Amitabh Bachchan saw failure at the peak of his career and struggled in a mature age as an actor and claimed back his throne of super-stardom in the second phase of his acting career.It is a little-known fact that in early life, Amitabh Bachchan, who aspired to become an actor, was rejected for his voice, gangly height and not having a ‘camera conducive face’. History is well-versed in irony because today, Amitabh Bachchan is known across India and the world at large for his baritone voice, commanding height and striking face.
+              </div>
+
+                 <img src="ab6.jpg" class="img-responsive img-fluid rounded-circle m-5">
+         </div>
+         <!-- extra content 4-->
+         <div class="media border mt-5">
+                 <img src="pc.jpg" class="img-responsive img-fluid rounded-circle m-5">
+              <div class="media-body pl-5 pt-5 pr-2" id="pc">
+                
+                <b id="pera">T</b>he Srivastava-Bachchan family (Hindi: बच्चन परिवार) is an Indian family, headed by Amitabh Bachchan.
+                The notable members of the family include Amitabh's parents, poet Harivansh Rai Bachchan and social activist Teji Bachchan; his wife, actress Jaya Bhaduri; their daughter Shweta Bachchan Nanda; their son, actor Abhishek Bachchan; and Abhishek's wife, actress Aishwarya Rai. In 2007, Time listed Amitabh Bachchan and Aishwarya Rai on its list of most influential Indians, while also noting that Abhishek does not garner the same level of adoration as his father Amitabh.[2]
+             </div>   
+         </div>
+         <!-- extra content 5-->
+          <div class="media border mt-4" id="songs">
+              <div class="media-body p-5 text-left" id="pc">
+               <b id="pera">A</b>ll India Radio for his ‘heavy voice’ - the baritone that everyone wants in their songs now.
+              And what’s a birthday party without songs? So, here we bring you the best of Amitabh Bachchan songs, in his own voice.
+              </div>   
+              <img src="song.jpg" class="img-responsive img-fluid rounded-circle mx-auto mt-5">
+              <div class=" media-body p-5 text-center" id="pc">
+                <b>1</b>. Mere Paas Aao (Mr Natwarlal, 1979)<br>
+                <b>2.</b> Rang Barse (Silsila, 1981)<br>
+                <b>3.</b> Neela Aasman (Silsila, 1981)<br>
+                <b>4.</b> Mere Angne Mein (Laawaris, 1981)<br>
+                <b>5.</b> Jahan Chaar Yaar (Sharaabi, 1984)  
+              </div> 
+         </div>
+
+       <!-- caoursel -->
+       <div id="demo" class="carousel slide mt-5" data-ride="carousel">
+               <ul class="carousel-indicators">
+                 <li data-target="#demo" data-slide-to="0" class="active"></li>
+                 <li data-target="#demo" data-slide-to="1"></li>
+                 <li data-target="#demo" data-slide-to="2"></li>
+               </ul>
+              <div class="carousel-inner">
+                 <div class="carousel-item active">
+                    <img class="img-responsive img-fluid" src="ab.jpg" alt="Los Angeles" width="1100" height="500">
+                 </div>
+                 <div class="carousel-item">
+                    <img class="img-responsive img-fluid" src="ab1.jpg" alt="Chicago" width="1100" height="500">
+                 </div>
+                 <div class="carousel-item">
+                    <img  class="img-responsive img-fluid" src="ab2.jpg" alt="New York" width="1100" height="500">
+                 </div>
+                 <div class="carousel-item">
+                   <img class="img-responsive img-fluid" src="ab3.jpg" alt="New York" width="1100" height="500">
+                 </div>
+              </div>
+              <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                 <span class="carousel-control-prev-icon"></span>
+              </a>
+              <a class="carousel-control-next" href="#demo" data-slide="next">
+                 <span class="carousel-control-next-icon"></span>
+              </a>
+        </div>
+       <!--  last content -->
+        <div class="container">
+       	 
+       	     <div class="row border mt-5">
+                <div class="col-xs-2 col-sm-3 col-md-4 col-lg-4 border pt-2">
+                   <h3 id="l1">Biography</h3>
+                   The Srivastava-Bachchan family is an Indian family, headed by Amitabh   Bachchan. The notable members of the family include Amitabh's parents, poet Harivansh Rai Bachchan and social activist Teji Bachchan; ... Wikipedia
+                   Current region: Mumbai
+                   Connected families: Kapoor family; Nanda family; Rai family
+                   Places of origin: Babupatti, Pratapgarh district, Uttar Pradesh, United Provinces of Agra and Oudh, British Raj.
+                </div>
+                <div class="col-xs-2 col-sm-3 col-md-4 col-lg-4 border pt-2">
+                 <h3>Filmography</h3>
+                   The notable members of the family include Amitabh's parents, poet Harivansh Rai Bachchan and social activist Teji Bachchan; his wife, actress Jaya Bhaduri; their daughter Shweta Bachchan Nanda; their son, actor Abhishek Bachchan; and Abhishek's wife, actress Aishwarya Rai.
+                   Bachchan family - Wikipedia
+                   https://en.wikipedia.org/wiki/Bachchan_fa.
+                </div>
+                <div class="col-xs-2 col-sm-3 col-md-4 col-lg-4 border pt-2">
+                 <h3>Links</h3>
+          	       https://en.wikipedia.org/wiki/Bachchan_family
+                   The notable members of the family include Amitabh's parents, poet Harivansh Rai Bachchan and social activist Teji Bachchan; his wife, actress Jaya Bhaduri; their daughter Shweta Bachchan Nanda; their son, actor Abhishek Bachchan; and Abhishek's wife, actress Aishwarya Rai.
+                   Amitabh Bachchan - Wikipedia.
+                </div>
+             </div>
+        </div>
+        <!-- icons-->
+
+        <div class="row mt-5">
+             <div class="col-md-4"></div>
+             <div class="col-md-4 text-center">
+                 <img src="fb.jpg">
+                 <img src="twitter.PNG">
+                 <img src="wsapp.png">
+                 <img src="mail.png">
+                 <img src="utube.png">
+                 <img src="in.png">
+             </div>
+             <div class="col-md-4"></div>
+        </div>
+
+        <div class="row mt-3 mb-4">
+           
+           <div class="col-md-4 text-left" style="font-size:10px;">
+              @some rights reserverd<br>
+              contact no. 9234512564
+           </div>
+           <div class="col-md-4 text-center" style="font-size:10px;">
+              @some rights reserverd<br>
+              contact no. 9234512564
+           </div>
+          <div class="col-md-4 text-right" style="font-size:10px;">
+              @some rights reserverd<br>
+              contact no. 9234512564
+           </div>
+        </div>
+
+</div>
+
+
+</body>
+</html>
